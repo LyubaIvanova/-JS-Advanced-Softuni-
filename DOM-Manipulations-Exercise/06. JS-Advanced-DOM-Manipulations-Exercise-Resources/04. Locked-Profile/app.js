19e112e219bb99cae034f50de8dc686c35f0c640
Lyuba Ivanova <tri_darveta@abv.bg>
2021-01-29 14:05:10 +0200

    Added solution

--- DOM-Manipulations-Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/04. Locked-Profile/app.js
+++ DOM-Manipulations-Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/04. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    [...document.querySelectorAll('button')].forEach(b => b.addEventListener('click', showHidden));
+
+    function showHidden() {
+        const parent = this.parentElement;
+        const unlock = parent.querySelector('input[value="unlock"]');
+        const hiddenSection = parent.querySelector('div');
+        
+        if(!unlock.checked){
+            return;
+        }
+        if (this.textContent === 'Hide it') {
+            hiddenSection.style.display = 'none';
+            this.textContent = 'Show more';
+        } else {
+            hiddenSection.style.display = 'inline-block';
+            this.textContent = 'Hide it';
+        }
+    }
 }
