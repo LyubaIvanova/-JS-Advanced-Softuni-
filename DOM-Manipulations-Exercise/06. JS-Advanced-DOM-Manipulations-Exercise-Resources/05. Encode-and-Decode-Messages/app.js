06fb005db12a44214c2a301566edc4c333e2c925
Lyuba Ivanova <tri_darveta@abv.bg>
2021-01-30 12:18:30 +0200

    Added solution

--- DOM-Manipulations-Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ DOM-Manipulations-Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [sendButton, decodeButton] = document.querySelectorAll('button');   //taking the buttons right away with destructuring;
+    const [senderField, recieverField] = document.querySelectorAll('textarea');  
+
+    sendButton.addEventListener('click', encodeAndSend);
+    decodeButton.addEventListener('click', receiveAndDecode);
+
+    function encodeAndSend() {
+        const message = senderField.value
+            .split('')
+            .map((char) => char.charCodeAt() + 1)    //using charCodeAt() without an index;
+            .map((char) => String.fromCharCode(char))
+            .join('');
+
+        recieverField.value = message;
+        senderField.value = '';
+    }
+
+    function receiveAndDecode() {
+        const message = recieverField.value
+            .split('')
+            .map((char) => char.charCodeAt() - 1)
+            .map((char) => String.fromCharCode(char))
+            .join('');
+
+        recieverField.value = message;
+    }
 }
