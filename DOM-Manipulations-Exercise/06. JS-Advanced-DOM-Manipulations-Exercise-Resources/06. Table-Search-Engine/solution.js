12871e6416e92b5e29e4ff02b646b1d85dfda72b
Lyuba Ivanova <tri_darveta@abv.bg>
2021-01-30 12:48:40 +0200

    Added solution

--- DOM-Manipulations-Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/06. Table-Search-Engine/solution.js
+++ DOM-Manipulations-Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/06. Table-Search-Engine/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-   //TODO...
+
+   document.querySelector('#searchBtn').addEventListener('click', onSearch);
+   const input = document.querySelector('#searchField');
+
+   function onSearch() {
+
+      const query = input.value.trim().toLocaleLowerCase();
+
+      if (query.trim().length > 0) {
+
+         const tbody = document.querySelector('tbody');
+         [...tbody.querySelectorAll('tr')].forEach(r => {
+            r.classList.remove('select')
+         });
+
+         [...tbody.querySelectorAll('td')].forEach(d => {
+            if (d.textContent.trim().toLocaleLowerCase().includes(query)) {
+
+               d.parentNode.classList.add('select');
+            }
+         });
+         input.value = '';
+      }
+   }
 }
